candy-staging/vendor_candy-preference

preference: Introduce {Global,Secure,System}MainSwitchPreference

src/com/android/settings/candy/preference/GlobalSettingMainSwitchPreference.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.candy.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class GlobalSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public GlobalSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public GlobalSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public GlobalSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            Settings.Global.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return Settings.Global.getInt(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0) != 0;
+        }
+    }
+}

src/com/android/settings/candy/preference/SecureSettingMainSwitchPreference.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.candy.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class SecureSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public SecureSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SecureSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SecureSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            Settings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return Settings.Secure.getInt(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0) != 0;
+        }
+    }
+}

src/com/android/settings/candy/preference/SystemSettingMainSwitchPreference.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.candy.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class SystemSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public SystemSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SystemSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SystemSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            Settings.System.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return Settings.System.getInt(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0) != 0;
+        }
+    }
+}

Project-Telescopium/vendor_custom-preference

Change-Id: I01e0f69569c7e024da7be3645de5096f34b3fac5

src/com/android/settings/custom/preference/GlobalSettingMainSwitchPreference.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.custom.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class GlobalSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public GlobalSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public GlobalSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public GlobalSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            Settings.Global.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return Settings.Global.getInt(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0) != 0;
+        }
+    }
+}

src/com/android/settings/custom/preference/SecureSettingMainSwitchPreference.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.custom.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class SecureSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public SecureSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SecureSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SecureSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            Settings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return Settings.Secure.getInt(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0) != 0;
+        }
+    }
+}

src/com/android/settings/custom/preference/SystemSettingMainSwitchPreference.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.custom.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class SystemSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public SystemSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SystemSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SystemSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            Settings.System.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return Settings.System.getInt(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0) != 0;
+        }
+    }
+}